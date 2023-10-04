0ee8ed273c434fd86fa2d6efc9bafc3177e44153
BaptisteV <baptiste.valle98@gmail.com>
2023-10-04 16:16:38 +0200

    Fix Private Value

--- FKO/Assets/BAV/Script/SpeedMaterialController.cs
+++ FKO/Assets/BAV/Script/SpeedMaterialController.cs
@@ -7,10 +7,10 @@ public class SpeedMaterialController : MonoBehaviour
 {
     public Material matSpeed;
     public bool enabledRenderer;
+    
     [Header("BlurControls")] 
     public float blurPower = 0.5f;
     public float blurPowerEffect = 0.5f;
-    private float currentBlurPower;
     
     [Header("Blur Oscillator")]
     public float minValue = 0.35f;
@@ -19,14 +19,19 @@ public class SpeedMaterialController : MonoBehaviour
     
     [Header("LinesControls")] 
     public float maskSizeLines = 0.1f;
-    private float currentMaskSizeLines;
     public float centerMaskEdge = 0.65f;
-    private float currentCenterMaskEdge;
     public Vector2 speedLineCenter = new Vector2(0.5f, 0.5f);
 
     public float lineCount = 20f;
-    private float currentLineCount;
     public float frameCount = 24f; 
+    
+    //Private float value
+    private float currentLineCount = 0f;
+    private float currentBlurPower = 0f;
+    private float currentMaskSizeLines = 0f;
+    private float currentCenterMaskEdge = 0f;
+    private float currentTime = 0f;
+    private float timeSinValue = 0f;
 
     public void Start()
     {
@@ -49,16 +54,12 @@ public class SpeedMaterialController : MonoBehaviour
         matSpeed.SetFloat("_FramesAnim", frameCount);
     }
     
-    
     // Update is called once per frame
     void Update()
     {
         IncrementTimeValue();
         SetValue();
     }
-    
-    private float currentTime = 0f;
-    private float timeSinValue = 0f;
 
     private void IncrementTimeValue()
     {
